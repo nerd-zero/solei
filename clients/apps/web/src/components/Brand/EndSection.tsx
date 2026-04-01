9e9237a8034239f68c6e191d3535ee338ffb8af3
Tadiwa Pfachi <pfachitadiwanashedavid@gmail.com>
2026-04-01 17:07:10 +0200

    Update EndSection.tsx

--- clients/apps/web/src/components/Brand/EndSection.tsx
+++ clients/apps/web/src/components/Brand/EndSection.tsx
@@ -3,7 +3,7 @@ import { PolarLogotype } from '../Layout/Public/PolarLogotype'
 export function EndSection() {
   return (
     <div className="flex h-full w-full flex-col items-center justify-center gap-6 p-12 md:p-64">
-      <PolarLogotype logoVariant="icon" size={80} />
+      <PolarLogotype className="h-20 w-20" />
     </div>
   )
 }
